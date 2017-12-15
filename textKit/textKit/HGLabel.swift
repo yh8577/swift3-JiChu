93fe7a15793b02908340420bfb6c9a0361fdf035
jyh <huig@jyhdeMacBook-Pro.local>
2017-12-15 23:55:45 +0800

    初始化 TextKit

---
+++ textKit/textKit/HGLabel.swift
@@ -0,0 +1,23 @@
+//
+//  HGLabel.swift
+//  textKit
+//
+//  Created by jyh on 2017/12/15.
+//  Copyright © 2017年 jyh. All rights reserved.
+//
+
+import UIKit
+
+
+/**
+ 1.使用 TextKit 接管 Label 的底层实现
+ 2.使用正则表达式过滤 URL
+ 3.交互
+*/
+class HGLabel: UILabel {
+
+
+    
+    
+
+}
---
+++ textKit/textKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  textKit
+//
+//  Created by jyh on 2017/12/15.
+//  Copyright © 2017年 jyh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
