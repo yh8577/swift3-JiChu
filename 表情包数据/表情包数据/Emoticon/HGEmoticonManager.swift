967c5a33027785385e95eb10fcbe16999506fa55
jyh <huig@jyhdeMacBook-Pro.local>
2017-12-15 23:10:35 +0800

    将方法封装到单例中

--- 表情包数据/表情包数据/Emoticon/HGEmoticonManager.swift
+++ 表情包数据/表情包数据/Emoticon/HGEmoticonManager.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 jyh. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // 表情管理器
 class HGEmoticonManager {
@@ -31,30 +31,30 @@ extension HGEmoticonManager {
     ///
     /// - Parameter string: 查找的条件字符串
     /// - Returns: 如果查找到返回表情模型,如果没有找到返回 nil
-    func findEmoticon(string: String) -> HGEmoticon? {
+    fileprivate func findEmoticon(string: String) -> HGEmoticon? {
         
         // 遍历表情包
         // OC 中过滤数组使用[谓词]
         // swift 中更简单
         for p in package {
             
-                // 在表情数组中过滤 string
-            
+            // 在表情数组中过滤 string
+            /*
             // 方法1
-//            let result = p.emoticons.filter({ (em) -> Bool in
-//                return em.chs == string
-//            })
+            let result = p.emoticons.filter({ (em) -> Bool in
+                return em.chs == string
+            })
             
             // 方法2 尾随闭包
-//            let result = p.emoticons.filter() { (em) -> Bool in
-//                return em.chs == string
-//            }
+            let result = p.emoticons.filter() { (em) -> Bool in
+                return em.chs == string
+            }
             // 方法3 如果闭包中只有一句,并且是返回
             // 闭包格式定义可以省略,使用$0-$1...依次替代原有参数
-//            let result = p.emoticons.filter() {
-//                return $0.chs == string
-//            }
-            
+            let result = p.emoticons.filter() {
+                return $0.chs == string
+            }
+            */
             // 方法4 如果闭包中只有一句,并且是返回
             // 闭包格式定义可以省略,使用$0-$1...依次替代原有参数
             // return 也可以省略
@@ -72,9 +72,44 @@ extension HGEmoticonManager {
 }
 
 // MARK: - 表情包数据处理
-fileprivate extension HGEmoticonManager {
+extension HGEmoticonManager {
     
-    func loadPackages() {
+    //  将给定的字符串转换成属性文本
+    func emoticonString(string: String, font: UIFont) -> NSAttributedString {
+        
+        let attrString = NSMutableAttributedString(string: string)
+        
+        // 建立正则表达式,过滤所有的表情文字
+        // [] () 都是正则表达式的关键字,如果要参与匹配,都需要转义
+        let pattern = "\\[.*?\\]"
+        
+        guard let regx = try? NSRegularExpression(pattern: pattern, options: []) else {
+            return attrString
+        }
+        
+        // 匹配所有项
+        let matches = regx.matches(in: string, options: [], range: NSRange(location: 0, length: attrString.length))
+        
+        // 遍历所有匹配结果
+        for m in matches.reversed() {
+            
+            let r = m.rangeAt(0)
+            let subStr = (attrString.string as NSString).substring(with: r)
+            
+            // 使用subStr 查找对应的符号
+            if let em = HGEmoticonManager.shared.findEmoticon(string: subStr) {
+                
+                attrString.replaceCharacters(in: r, with: em.imageText(font: font))
+            }
+        }
+        
+        // 统一设置一遍字符串的属性
+        attrString.addAttributes([NSFontAttributeName: font], range: NSRange(location: 0, length: attrString.length))
+        
+        return attrString
+    }
+    
+    fileprivate func loadPackages() {
         
         guard let path = Bundle.main.path(forResource: "HMEmoticon.bundle", ofType: nil),
               let bunole = Bundle(path: path),
--- 表情包数据/表情包数据/ViewController.swift
+++ 表情包数据/表情包数据/ViewController.swift
@@ -35,59 +35,59 @@ class ViewController: UIViewController {
          必须使用倒序替换
          **/
         //
-        let str = "我[爱你]啊[笑哈哈]!"
-        label.attributedText = emoticonString(string: str)
+        let str = "我[爱你]啊[笑哈哈]![马上到]"
+        label.attributedText = HGEmoticonManager.shared.emoticonString(string: str, font: label.font)
 
     }
     
-    //  将给定的字符串转换成属性文本
-    func emoticonString(string: String) -> NSAttributedString {
-        
-        let attrString = NSMutableAttributedString(string: string)
-        
-        // 建立正则表达式,过滤所有的表情文字
-        // [] () 都是正则表达式的关键字,如果要参与匹配,都需要转义
-        let pattern = "\\[.*?\\]"
-        
-        guard let regx = try? NSRegularExpression(pattern: pattern, options: []) else {
-            return attrString
-        }
-        
-        // 匹配所有项
-        let matches = regx.matches(in: string, options: [], range: NSRange(location: 0, length: attrString.length))
-        
-        // 遍历所有匹配结果
-        for m in matches.reversed() {
-            
-            let r = m.rangeAt(0)
-            let subStr = (attrString.string as NSString).substring(with: r)
-            
-            // 使用subStr 查找对应的符号
-            if let em = HGEmoticonManager.shared.findEmoticon(string: subStr) {
-                
-                attrString.replaceCharacters(in: r, with: em.imageText(font: label.font))
-            }
-        }
-        return attrString
-    }
+//    //  将给定的字符串转换成属性文本
+//    func emoticonString(string: String) -> NSAttributedString {
+//        
+//        let attrString = NSMutableAttributedString(string: string)
+//        
+//        // 建立正则表达式,过滤所有的表情文字
+//        // [] () 都是正则表达式的关键字,如果要参与匹配,都需要转义
+//        let pattern = "\\[.*?\\]"
+//        
+//        guard let regx = try? NSRegularExpression(pattern: pattern, options: []) else {
+//            return attrString
+//        }
+//        
+//        // 匹配所有项
+//        let matches = regx.matches(in: string, options: [], range: NSRange(location: 0, length: attrString.length))
+//        
+//        // 遍历所有匹配结果
+//        for m in matches.reversed() {
+//            
+//            let r = m.rangeAt(0)
+//            let subStr = (attrString.string as NSString).substring(with: r)
+//            
+//            // 使用subStr 查找对应的符号
+//            if let em = HGEmoticonManager.shared.findEmoticon(string: subStr) {
+//                
+//                attrString.replaceCharacters(in: r, with: em.imageText(font: label.font))
+//            }
+//        }
+//        return attrString
+//    }
     
     
     
     
-    func demo() {
-        
-        let emoti = HGEmoticonManager.shared
-        
-        //        print(emoti.package.last?.emoticons.first?.image)
-        //        print(emoti.package.last?.emoticons.first)
-        
-        
-        //        print(emoti.findEmoticon(string: "[笑哈哈]"))
-        
-        let em = emoti.findEmoticon(string: "[笑哈哈]")
-        
-        label.attributedText = em?.imageText(font: label.font)
-    }
+//    func demo() {
+//        
+//        let emoti = HGEmoticonManager.shared
+//        
+//        //        print(emoti.package.last?.emoticons.first?.image)
+//        //        print(emoti.package.last?.emoticons.first)
+//        
+//        
+//        //        print(emoti.findEmoticon(string: "[笑哈哈]"))
+//        
+//        let em = emoti.findEmoticon(string: "[笑哈哈]")
+//        
+//        label.attributedText = em?.imageText(font: label.font)
+//    }
 
 }
 
